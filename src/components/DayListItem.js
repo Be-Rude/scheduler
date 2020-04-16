e134f529f4052ab6b67a2564f0610bec0f744812
Brad Ruud <bradruud@Brads-MacBook-Pro.local>
2020-04-15 17:14:43 -0700

    add several Appointment form components

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,6 +8,26 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
   });
+  let currentSpots = "";
+  const formatSpots = () => {
+    
+    if (props.spots === 2) {
+      currentSpots = "2 spots remaining"
+      return currentSpots;
+    } else
+    if (props.spots === 1) {
+      currentSpots = "1 spot remaining"
+      return currentSpots;
+    } else
+    if (props.spots === 0) {
+      currentSpots = "no spots remaining"
+      return currentSpots;
+    } else
+    currentSpots = props.spots;
+    return currentSpots;
+  }
+
+  formatSpots();
 
 
   return (
@@ -16,7 +36,7 @@ export default function DayListItem(props) {
     onClick={() => props.setDay(props.name)}
     >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{currentSpots}</h3>
     </li>
   );
 }
