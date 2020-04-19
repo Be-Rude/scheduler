dea0d3d056a53febe8434f9cc953f774eaa50fb7
Brad Ruud <bradruud@Brads-MacBook-Pro.local>
2020-04-18 17:46:05 -0700

    add getAppointmentsForDay function

---
+++ src/helpers/selectors.js
@@ -0,0 +1,22 @@
+
+
+export function getAppointmentsForDay(state, day) {
+  const results = [];
+  const filteredDays = state.days.filter(each => each.name === day);
+
+
+  if (filteredDays.length === 0) {
+    results.push();
+    return results;
+  } 
+
+  for (let each of filteredDays) {
+    if (each.name === day){
+      let apptVals = each.appointments.values();
+      for (let apptVal of apptVals) {
+        results.push(state.appointments[apptVal])
+      }
+       return results;
+    }
+  }
+}
